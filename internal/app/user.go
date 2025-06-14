ae6957b9b9d1c4e2ea239944938bb90a15664449
WilliamKSilva <wilz.kelvin12@gmail.com>
2025-06-14 16:26:06 -0300

    feat: add implementation of UUID generator, password hashing and user repository on user service

--- internal/app/user.go
+++ internal/app/user.go
@@ -1,11 +1,34 @@
 package app
 
-import "github.com/WilliamKSilva/go-hexagonal/internal/domain"
+import (
+	"fmt"
+
+	"github.com/WilliamKSilva/go-hexagonal/internal/domain"
+)
 
 type UserService struct {
 	domain.UserService
+
+	userRepo domain.UserRepository
+	uuidGen  domain.UUIDGenerator
+	crypt    domain.Crypt
 }
 
-func (userService *UserService) Create(name string, email string, role domain.UserRole) (*domain.User, error) {
-	return nil, nil
+func (s *UserService) Create(name string, password string, email string, role domain.UserRole) (*domain.User, error) {
+	uuid, err := s.uuidGen.Generate()
+	if err != nil {
+		return nil, fmt.Errorf("error trying to generate a new UUID for the user: %w", err)
+	}
+
+	hash, err := s.crypt.Encrypt(password)
+	if err != nil {
+		return nil, fmt.Errorf("error trying to encrypt user password: %w", err)
+	}
+
+	user, err := s.userRepo.Save(uuid, name, hash, email, role)
+	if err != nil {
+		return nil, fmt.Errorf("error trying to save user on the database: %w", err)
+	}
+
+	return user, nil
 }
