4eda180a61e24ebab80f6963900d717a673ac803
WilliamKSilva <wilz.kelvin12@gmail.com>
2025-06-15 08:42:31 -0300

    feat: add room delete method to repository

--- internal/domain/repositories/room_repository.go
+++ internal/domain/repositories/room_repository.go
@@ -5,5 +5,6 @@ import "github.com/WilliamKSilva/go-hexagonal/internal/domain"
 type RoomRepository interface {
 	Save(domain.Room) (*domain.Room, error)
 	Update(name *string, capacity *int32, status *domain.RoomStatus, maintenanceNote *string) error
+	Delete(uuid string) error
 	SearchByUUID(uuid string) (*domain.Room, error)
 }
